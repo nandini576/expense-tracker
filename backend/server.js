25e75ca1804abf6a1781bd3182f994b9d8ed0f7d
Nandini Kona <nandukona12@gmail.com>
2026-05-25 21:56:37 +0530

    Fixed backend API routes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ import riskRoutes from "./routes/riskRoutes.js";
 dotenv.config();
 connectDB();
 const app=express();
-app.use(cors());
+app.use(cors({
+  origin: "https://expense-tracker-opal-six-75.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
